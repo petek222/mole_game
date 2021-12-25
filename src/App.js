6c03164ae907ee7a5ca4c5d794d2d13f26f2b900
petek222 <peter.e.koncelik@vanderbilt.edu>
2021-12-25 15:02:37 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import logo from './logo.svg';
+import mole from './mole.jpg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+class FormComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.solution = "ABC";
+    this.coordinates = `41°27'53"N 81°53'13"W`
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    if (this.state.value == this.solution) {
+      alert(`You Solved the puzzle! Please call the supplied phone number, then navigate to the following coordinates: ${this.coordinates}`)
+    }
+    else {
+      alert("I'm sorry, that answer is incorrect. Please try again.")
+    }
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={mole} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <header>Please Enter the Secret Word</header>
+        <FormComponent/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
